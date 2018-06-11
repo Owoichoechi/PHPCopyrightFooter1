c1d9018ca1cddfbbed0c3b9eaa7a2a8b9b9f1a52
Yashasvi Jaiswal <yashasvijaiswal@gmail.com>
2018-06-11 23:55:37 +0530

    Updated.

---
+++ PHPCopyrightFooter1.php
@@ -0,0 +1,13 @@
+<footer id="footer">
+    <div class="container">
+        <p>&copy; <?php echo date("Y");?> | PHP Copyright Footer 1 is created by <a href="https://about.me/YashasviJaiswal" target="_blank">Yashasvi Jaiswal</a>.</p>
+    </div>
+</footer>
+
+<!--
+<footer id="footer">
+    <div class="container">
+        <p>&copy; <?php //echo date("Y");?> | PHP Copyright Footer 1 is created by <a href="https://about.me/YashasviJaiswal" target="_blank">Yashasvi Jaiswal</a>.</p>
+    </div>
+</footer>
+-->
